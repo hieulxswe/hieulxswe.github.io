8f9728111079f50bcce43b69c326abd237f66331
Hieu Xuan Leu <hieulx.swe@gmail.com>
2024-07-19 22:57:35 +1000

    a

--- assets/js/ephesus.js
+++ assets/js/ephesus.js
@@ -1,12 +1,12 @@
 function toggleNightMode(){
 	if(document.documentElement.getAttribute('data-theme') == 'dark'){
 		document.documentElement.setAttribute('data-theme', 'light');
-		document.getElementById('mode-switcher').classList.add('active');
+		// document.getElementById('mode-switcher').classList.add('active');
 		// localStorage.setItem("theme","dark");
 	}
 	else{
 		document.documentElement.setAttribute('data-theme', 'light');
-		document.getElementById('mode-switcher').classList.remove('active');
+		// document.getElementById('mode-switcher').classList.remove('active');
 		// localStorage.setItem("theme","");
 	}
 }

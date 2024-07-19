4104013c71c312a6ff9c522216a4e9edcd44e1f0
Hieu Xuan Leu <hieulx.swe@gmail.com>
2024-07-19 22:52:25 +1000

    disable toggleNightMode

--- assets/js/ephesus.js
+++ assets/js/ephesus.js
@@ -2,10 +2,10 @@ function toggleNightMode(){
 	if(document.documentElement.getAttribute('data-theme') == 'dark'){
 		document.documentElement.setAttribute('data-theme', 'light');
 		document.getElementById('mode-switcher').classList.add('active');
-		localStorage.setItem("theme","light");
+		localStorage.setItem("theme","dark");
 	}
 	else{
-		document.documentElement.setAttribute('data-theme', 'dark');
+		document.documentElement.setAttribute('data-theme', 'light');
 		document.getElementById('mode-switcher').classList.remove('active');
 		localStorage.setItem("theme","");
 	}
